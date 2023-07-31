cdb4bc769798b770dab7015e1c0dca24871ed162
Seth <58447309+ohmygaugh-crypto@users.noreply.github.com>
2023-07-31 20:34:28 +0000

    changed webcopy again

--- pages/index.js
+++ pages/index.js
@@ -22,6 +22,7 @@ import thumbBlackbaud from '../public/images/links/blackbaudsocialgoodstartup.pn
 import thumbThrivacy from '../public/images/works/Thrivacycommunity.jpg'
 import Image from 'next/image'
 
+
 const ProfileImage = chakra(Image, {
   shouldForwardProp: prop => ['width', 'height', 'src', 'alt'].includes(prop)
 })
@@ -37,226 +38,226 @@ const ProfileImage = chakra(Image, {
 const Home = () => (
   <Layout>
     <Container>
-      <Box
-        borderRadius="lg"
-        mb={6}
-        p={3}
-        textAlign="center"
-        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-        css={{ backdropFilter: 'blur(10px)' }}
-      >
-        Hello, I am a product strategist based in the USA!
-      </Box>
-
-      <Box display={{ md: 'flex' }}>
-        <Box flexGrow={1}>
-          <Heading as="h2" variant="page-title">
-            Seth McGaugh
-          </Heading>
-          <p>Digital Native ( Product Strategist / Manager / Developer / Designer )</p>
-        </Box>
         <Box
-          flexShrink={0}
-          mt={{ base: 4, md: 0 }}
-          ml={{ md: 6 }}
+          borderRadius="lg"
+          mb={6}
+          p={3}
           textAlign="center"
+          bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+          css={{ backdropFilter: 'blur(10px)' }}
         >
+          Hello, I am a product strategist based in the USA!
+        </Box>
+
+        <Box display={{ md: 'flex' }}>
+          <Box flexGrow={1}>
+            <Heading as="h2" variant="page-title">
+              Seth McGaugh
+            </Heading>
+            <p>Digital Native ( Product Strategist / Manager / Developer / Designer )</p>
+          </Box>
           <Box
-            borderColor="whiteAlpha.800"
-            borderWidth={2}
-            borderStyle="solid"
-            w="100px"
-            h="100px"
-            display="inline-block"
-            borderRadius="full"
-            overflow="hidden"
+            flexShrink={0}
+            mt={{ base: 4, md: 0 }}
+            ml={{ md: 6 }}
+            textAlign="center"
           >
-            <ProfileImage
-              src="/images/linkedinHEADSHOT.png"
-              alt="Profile image"
+            <Box
+              borderColor="whiteAlpha.800"
+              borderWidth={2}
+              borderStyle="solid"
+              w="100px"
+              h="100px"
+              display="inline-block"
               borderRadius="full"
-              width="100%"
-              height="100%"
-            />
+              overflow="hidden"
+            >
+              <ProfileImage
+                src="/images/linkedinHEADSHOT.png"
+                alt="Profile image"
+                borderRadius="full"
+                width="100%"
+                height="100%"
+              />
+            </Box>
           </Box>
         </Box>
-      </Box>
-      <Section></Section>
+        <Section></Section>
 
-      <Section delay={0.1}>
-        <Heading as="h3" variant="section-title">
-          Work
-        </Heading>
-        <Paragraph>
-        {'"A Jack of all trades master of none, is better than a master of one."'}
-        </Paragraph>
-        <br> 
-        
-        </br>
-        <Paragraph>
-        {'Seth pretty much lives by this mantra. Choosing the startup path made him wear many hats over the years. That pretty much hasn`&apos;`t changed since. He primarily lead the product roadmap for Thrivacy. There he often had to get his hands dirty with UIUX design, Fullstack Development, and DevOps when there was pretty much no cashflow to pay for developer assistance. That is life. Pivoting from finance to software development. He has found his groove. His mission is to lead dev teams towards building delightful, human-centered, software UIUX so, that one day, he will run his own company when the time is right. Until then he is going to listen, take notes, manage expectations, get teams fired up and aligned to build something awesome.'} 
-        </Paragraph>
-        
-      </Section>
-
-      <Section delay={0.2}>
-        <Heading as="h3" variant="section-title">
-          Bio
-        </Heading>
-        <BioSection>
-          <BioYear>2019</BioYear>
-          Completed the{' '} 
-          <Link href="https://sc.edu/study/colleges_schools/moore/study/international_business/degree_programs/bachelors/curriculum/cohort_programs/ibce_hong_kong/index.php">
-          IBCE{' '} 
-          </Link>
-            Program within the Darla Moore School of
-          Business at the Chinese University of Hong Kong. Also, worked at a seed stage FinTech startup called{' '}
-          <Link href="https://www.gini.co/">
-            gini.{' '}
-          </Link>     
+        <Section delay={0.1}>
+          <Heading as="h3" variant="section-title">
+            Work
+          </Heading>
+          <Paragraph>
+          {'"A Jack of all trades master of none, is better than a master of one."'}
+          </Paragraph>
+          <br> 
           
+          </br>
+          <Paragraph>
+          {'Seth pretty much lives by this mantra. Choosing the startup path made him wear many hats over the years. That hasn`t changed much. He often has to get his hands dirty with UIUX design, Fullstack Development, and DevOps. Pivoting from finance to software development. He has found his groove. His mission is to lead dev teams towards building delightful, human-centered, software products. When the time is right, he intends to run his own company. Until then, he is going to listen, take notes, align expectations, and get teams fired up to build something awesome.'} 
+          </Paragraph>
           
-        </BioSection>
-        <BioSection>
-          <BioYear>2020</BioYear>
-          Earned 3rd place in the University of South Carolina {' '}
-          <Link href="https://www.sc.edu/about/offices_and_divisions/provost/academicpriorities/undergradstudies/mcnair_blockchain.php">
-          McNair Institute Blockchain Challenge
-          </Link>{' '}
-        </BioSection>
-        <BioSection>
-          <BioYear>2020</BioYear>
-          Co-founded a startup called{' '}
-          <Link href="https://www.thrivacy.io/why-thrivacy">
-          THRIVACY!{' '}
-          </Link>
-          A Web3 Wallet & Privacy Platform based in Columbia, SC - USA.
-        </BioSection>
-        <BioSection>
-          <BioYear>2021</BioYear>
-          Accepted into the{' '}
-          <Link href="https://www.blackbaud.com/socialgoodaccelerator">
-          Blackbaud Social Good Accelerator.
-          </Link>
-          {' '}As well as {' '}
-          <Link href="https://www.velocitynetwork.foundation/member_category/general">
-          The Velocity Network Foundation.
-          </Link>
-        </BioSection>
-        <BioSection>
-          <BioYear>2022</BioYear>
-          Accepted into the{' '}
-          <Link href="https://vilcap.com/current-programs/ibm-hyper-protect-accelerator">
-            Village Capital Hyper Accelerator.
-          </Link>
-        </BioSection>
-        <BioSection>
-          <BioYear>2023</BioYear>
-          Worked as a Product Strategist at{' '}
-          <Link href="https://www.anthroware.com/">
-            Anthroware.
-          </Link>
-        </BioSection>
-        <BioSection>
-          <BioYear>Currently</BioYear>
-          Looking for work enabling human-centered UIUX as a Product Strategist/Manager 👨‍💻
-        </BioSection>
-        <BioSection>
-          <BioYear>TBA</BioYear>
-          Portfolio coming soon...  
-        </BioSection>
-      </Section>
+        </Section>
 
-      <Section delay={0.3}>
-        <Heading as="h3" variant="section-title">
-          I ♥
-        </Heading>
-        <Paragraph>
-            Product Strategy. It primes me to listen first, speak less, saying `&ldquo;`NO`&ldquo;` and taking action decisively. If I am not staring at a screen all day I enjoy: Cooking, Pickleball, and the Hunting/Fishing. When I am staring at a screen all day I am building on the latest tools like:{' '}
-          <Link href="https://www.validide.io/" target="_blank">
-          A-Frame,{' '} 
-          </Link>
-          <Link href="https://www.obsidian.com" target="_blank">
-             obsidian{' '}
-          </Link>
-          and{' '}
-          <Link href="https://www.logseq.com" target="_blank">
-             Logseq.
-          </Link>
-        </Paragraph>
-
-      </Section>
-
-      <Section delay={0.3}>
-        <Heading as="h3" variant="section-title">
-          On the web
-        </Heading>
-        <List>
-          <ListItem>
-            <Link href="https://github.com/ohmygaugh-crypto" target="_blank">
-              <Button
-                variant="ghost"
-                colorScheme="teal"
-                leftIcon={<IoLogoGithub />}
-              >
-                @ohmygaugh-crypto
-              </Button>
+        <Section delay={0.2}>
+          <Heading as="h3" variant="section-title">
+            Bio
+          </Heading>
+          <BioSection>
+            <BioYear>2019</BioYear>
+            Completed the{' '} 
+            <Link href="https://sc.edu/study/colleges_schools/moore/study/international_business/degree_programs/bachelors/curriculum/cohort_programs/ibce_hong_kong/index.php">
+            IBCE{' '} 
             </Link>
-          </ListItem>
-          <ListItem>
-            <Link href="https://www.linkedin.com/in/seth-mcgaugh/" target="_blank">
-              <Button
-                variant="ghost"
-                colorScheme="teal"
-                leftIcon={<IoLogoLinkedin />}
-              >
-                @seth-mcgaugh (contact me here)
-              </Button>
+              Program within the Darla Moore School of
+            Business at the Chinese University of Hong Kong. Also, worked at a seed stage FinTech startup called{' '}
+            <Link href="https://www.gini.co/">
+              gini.{' '}
+            </Link>     
+            
+            
+          </BioSection>
+          <BioSection>
+            <BioYear>2020</BioYear>
+            Earned 3rd place in the University of South Carolina {' '}
+            <Link href="https://www.sc.edu/about/offices_and_divisions/provost/academicpriorities/undergradstudies/mcnair_blockchain.php">
+            McNair Institute Blockchain Challenge
+            </Link>{' '}
+          </BioSection>
+          <BioSection>
+            <BioYear>2020</BioYear>
+            Co-founded a startup called{' '}
+            <Link href="https://www.thrivacy.io/why-thrivacy">
+            THRIVACY!{' '}
             </Link>
-          </ListItem>
-          <ListItem>
-            <Link href="https://twitter.com/seth_mcgaugh" target="_blank">
-              <Button
-                variant="ghost"
-                colorScheme="teal"
-                leftIcon={<IoLogoTwitter />}
-              >
-                @seth_mcgaugh
-              </Button>
+            A Web3 Wallet & Privacy Platform based in Columbia, SC - USA.
+          </BioSection>
+          <BioSection>
+            <BioYear>2021</BioYear>
+            Accepted into the{' '}
+            <Link href="https://www.blackbaud.com/socialgoodaccelerator">
+            Blackbaud Social Good Accelerator.
             </Link>
-          </ListItem>
-          <ListItem>
-            <Link href="https://www.linkedin.com/in/seth-mcgaugh/" target="_blank">
-              <Button
-                variant="ghost"
-                colorScheme="teal"
-                leftIcon={<IoLogoInstagram />}
-              >
-                @seth-mcgaugh
-              </Button>
+            {' '}As well as {' '}
+            <Link href="https://www.velocitynetwork.foundation/member_category/general">
+            The Velocity Network Foundation.
             </Link>
-          </ListItem>
-        </List>
+          </BioSection>
+          <BioSection>
+            <BioYear>2022</BioYear>
+            Accepted into the{' '}
+            <Link href="https://vilcap.com/current-programs/ibm-hyper-protect-accelerator">
+              Village Capital Hyper Accelerator.
+            </Link>
+          </BioSection>
+          <BioSection>
+            <BioYear>2023</BioYear>
+            Worked as a Product Strategist at{' '}
+            <Link href="https://www.anthroware.com/">
+              Anthroware.
+            </Link>
+          </BioSection>
+          <BioSection>
+            <BioYear>Currently</BioYear>
+            Looking for work that embarks on enabling human-centered UIUX as a Product Strategist/Manager 👨‍💻, or to assist great products as a Sales Engineer.
+          </BioSection>
+          <BioSection>
+            <BioYear>TBA</BioYear>
+            Portfolio coming soon...  
+          </BioSection>
+        </Section>
 
-        <SimpleGrid columns={[1, 2, 2]} gap={6}>
-          <GridItem
-            href="https://www.blackbaud.com/newsroom/article/2021/12/29/blackbaud-s-social-good-startup-program-brings-innovative-tech-solutions-to-mission-based-organizations"
-            title="Startup for Social Good"
-            thumbnail={thumbBlackbaud}
-          >
-            My Mission 
-          </GridItem>
-          <GridItem
-            href="https://www.thrivacy.io/"
-            title="THRIVACY"
-            thumbnail={thumbThrivacy}
-          >
-            A Privacy Company
-          </GridItem>
-        </SimpleGrid>
+        <Section delay={0.3}>
+          <Heading as="h3" variant="section-title">
+            I ♥
+          </Heading>
+          <Paragraph>
+              Product Strategy. It primes me to listen first, speak less, saying 'NO' more, and taking action(s) decisively. When I'm not staring at screens professionally I enjoy: Cooking, Pickleball, and the Hunting/Fishing. As a hobbyist I am building on the latest tools like:{' '}
+            <Link href="https://www.validide.io/" target="_blank">
+            A-Frame,{' '} 
+            </Link>
+            <Link href="https://www.obsidian.com" target="_blank">
+              obsidian{' '}
+            </Link>
+            and{' '}
+            <Link href="https://www.logseq.com" target="_blank">
+              Logseq.
+            </Link>
+          </Paragraph>
 
-        
-      </Section>
+        </Section>
+
+        <Section delay={0.3}>
+          <Heading as="h3" variant="section-title">
+            On the web
+          </Heading>
+          <List>
+            <ListItem>
+              <Link href="https://github.com/ohmygaugh-crypto" target="_blank">
+                <Button
+                  variant="ghost"
+                  colorScheme="teal"
+                  leftIcon={<IoLogoGithub />}
+                >
+                  @ohmygaugh-crypto
+                </Button>
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link href="https://www.linkedin.com/in/seth-mcgaugh/" target="_blank">
+                <Button
+                  variant="ghost"
+                  colorScheme="teal"
+                  leftIcon={<IoLogoLinkedin />}
+                >
+                  @seth-mcgaugh (contact me here)
+                </Button>
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link href="https://twitter.com/seth_mcgaugh" target="_blank">
+                <Button
+                  variant="ghost"
+                  colorScheme="teal"
+                  leftIcon={<IoLogoTwitter />}
+                >
+                  @seth_mcgaugh
+                </Button>
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link href="https://www.linkedin.com/in/seth-mcgaugh/" target="_blank">
+                <Button
+                  variant="ghost"
+                  colorScheme="teal"
+                  leftIcon={<IoLogoInstagram />}
+                >
+                  @seth-mcgaugh
+                </Button>
+              </Link>
+            </ListItem>
+          </List>
+
+          <SimpleGrid columns={[1, 2, 2]} gap={6}>
+            <GridItem
+              href="https://www.blackbaud.com/newsroom/article/2021/12/29/blackbaud-s-social-good-startup-program-brings-innovative-tech-solutions-to-mission-based-organizations"
+              title="Startup for Social Good"
+              thumbnail={thumbBlackbaud}
+            >
+              My Mission 
+            </GridItem>
+            <GridItem
+              href="https://www.thrivacy.io/"
+              title="THRIVACY"
+              thumbnail={thumbThrivacy}
+            >
+              A Privacy Company
+            </GridItem>
+          </SimpleGrid>
+
+          
+        </Section>
     </Container>
   </Layout>
 )
