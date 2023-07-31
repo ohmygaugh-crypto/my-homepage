df653b9aba084956e59c9333debde035845362e8
Seth <58447309+ohmygaugh-crypto@users.noreply.github.com>
2023-07-31 17:02:38 +0000

    wow I was missing the h- in "href" on line 177

--- pages/index.js
+++ pages/index.js
@@ -174,7 +174,7 @@ const Home = () => (
           <Link href="https://www.validide.io/" target="_blank">
           A-Frame,{' '} 
           </Link>
-          <Link ref="https://www.obsidian.com" target="_blank">
+          <Link href="https://www.obsidian.com" target="_blank">
              obsidian{' '}
           </Link>
           and{' '}
